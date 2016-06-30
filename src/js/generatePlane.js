80731a60c7d6df1918bba46e651b94d0ae1ea076
Thomas Wilburn <news@thomaswilburn.net>
2016-06-30 15:54:53 -0700

    Detail view!

---
+++ src/js/generatePlane.js
@@ -0,0 +1,21 @@
+var { transform, transformOrigin } = require("./lib/prefixed");
+
+const NS = "http://www.w3.org/2000/svg";
+
+module.exports = function(group, id, isDorsal) {
+  var offset = group.getBBox();
+  var image = document.createElementNS(NS, "svg");
+  image.setAttribute("viewBox", `${offset.x} ${offset.y} ${offset.width} ${offset.height}`);
+  image.setAttribute("xlmns", NS);
+  image.setAttribute("width", offset.width);
+  image.setAttribute("height", offset.height);
+  image.setAttribute("preserveAspectRatio", "xMidYMid meet");
+  image.appendChild(group);
+  image.setAttribute("class", "plane");
+  image.setAttribute("id", id);
+  image.setAttribute("data-view", isDorsal ? "dorsal" : "side");
+  image.style[transform] = "translate3d(0, 0, 0) rotate(0)";
+  image.style[transformOrigin] = "50% 50%";
+  image.bounds = offset;
+  return image;
+};
--- src/js/main.js
+++ src/js/main.js
@@ -2,88 +2,82 @@
 // require("./lib/ads");
 // var track = require("./lib/tracking");
 
-var container = document.querySelector(".svg-container");
-
 var $ = require("./lib/qsa");
 var xhr = require("./lib/xhr");
 var closest = require("./lib/closest");
+var dot = require("./lib/dot");
 var { transform, transformOrigin } = require("./lib/prefixed");
 
-const NS = "http://www.w3.org/2000/svg";
+var generatePlaneLayer = require("./generatePlane");
+var planeHTML = dot.compile(require("./_planeDetail.html"));
+
+var container = document.querySelector(".svg-container");
+var aside = document.querySelector("aside.explore");
+var frame = aside.querySelector(".plane-frame");
 const stripView = /_(dorsal|side)_view/;
 
-var generatePlaneLayer = function(group, id, isDorsal) {
-  var offset = group.getBBox();
-  var image = document.createElementNS(NS, "svg");
-  image.setAttribute("viewBox", `${offset.x} ${offset.y} ${offset.width} ${offset.height}`);
-  image.setAttribute("xlmns", NS);
-  image.setAttribute("width", offset.width);
-  image.setAttribute("height", offset.height);
-  image.setAttribute("preserveAspectRatio", "xMidYMid meet");
-  image.appendChild(group);
-  image.setAttribute("class", "plane");
-  image.setAttribute("id", id);
-  image.setAttribute("data-view", isDorsal ? "dorsal" : "side");
-  image.style[transform] = "translate3d(0, 0, 0) rotate(0)";
-  image.style[transformOrigin] = "50% 50%";
-  image.bounds = offset;
-  return image;
-};
-
+// Load the planes and generate layers
 xhr("./assets/planes.svg", function(err, data) {
   container.innerHTML = data;
 
   var svg = container.querySelector("svg");
-
   var groups = $("g[id]", svg);
-  
-  var sprites = {};
+
+  // Extract each group and add to a sprite map  
+  var views = {};
   groups.forEach(function(group) {
     var id = group.getAttribute("id");
     var isDorsal = id.match(/dorsal/i);
     var key = id.replace(stripView, "");
     var sprite = generatePlaneLayer(group, id, isDorsal);
     group.removeAttribute("id");
-    if (!sprites[key]) sprites[key] = {};
-    sprites[key][isDorsal ? "dorsal" : "side"] = sprite;
-    // sprite.element.appendChild(style.cloneNode());
-    container.appendChild(sprite);
-    if (isDorsal) sprite.style.display = "none";
+    if (!views[key]) views[key] = {};
+    //assign dorsal and side views to their proper places
+    if (isDorsal) {
+      var wrapper = document.createElement("div");
+      wrapper.className = "plane-wrapper";
+      frame.appendChild(sprite);
+      views[key].dorsal = sprite;
+      sprite.classList.add("dorsal");
+      sprite.style[transform] = "rotate(-90deg)";
+    } else {
+      container.appendChild(sprite);
+      views[key].side = sprite;
+      sprite.classList.add("side");
+    }
   });
 
+  // Turn off the original SVG
   svg.style.display = "none";
-
-  // svg.parentElement.removeChild(svg);
-
-  window.sprites = sprites;
 
   var reposition = function(plane) {
     plane.x = Math.random() * -800;
     plane.y = Math.random() * window.innerHeight * .8 + 50;
     plane.dx = Math.random() * 30 + 30;
-    plane.r = Math.random() * -4;
+    plane.r = Math.random() * -5;
   }
 
-  var planes = Object.keys(sprites).map(function(d) {
-    var sprite = sprites[d];
-    var position = {
+  // Place the planes in the sky for flyby animations
+  var planes = Object.keys(views).map(function(d) {
+    var sprite = views[d];
+    var plane = {
       x: 0,
       y: 0,
       dx: 0,
       dy: 0,
       r: -4,
       sprite: sprite.side,
-      views: sprite,
       data: planeDetail[d]
     };
     if (!planeDetail[d]) console.log("Missing plane data: ", d);
-    reposition(position);
-    return position;
+    reposition(plane);
+    return plane;
   });
 
+  //run animations
   var last;
   var timeout;
-  var update = function(now) {
+  var animate = function(now) {
     var elapsed = (now - (last || now)) / 1000;
     last = now;
     var bounds = container.getBoundingClientRect();
@@ -92,25 +86,67 @@ xhr("./assets/planes.svg", function(err, data) {
       s.y += elapsed * s.dy;
       s.sprite.style[transform] = `translate3d(${s.x}px, ${s.y}px, 0) rotate(${s.r}deg)`;
       if (s.x > bounds.width || s.y > window.innerHeight * .8) {
-        s.x = Math.random() * -800 - s.sprite.bounds.width;
-        s.y = Math.random() * window.innerHeight * .8 + 50;
+        reposition(s);
       }
     });
-    timeout = requestAnimationFrame(update);
+    timeout = requestAnimationFrame(animate);
   };
 
-  var start = function() {
+  var startAnimation = function() {
     last = 0;
-    timeout = requestAnimationFrame(update);
+    timeout = requestAnimationFrame(animate);
   }
 
-  start();
+  startAnimation();
 
-  container.addEventListener("click", function(e) {
-    var image = closest(e.target, "svg[id]");
+  // Define the exploration interface
+
+  var onScroll = function() {
+    var bounds = aside.getBoundingClientRect();
+    if (bounds.top < window.innerHeight / 2) {
+      if (!timeout) return;
+      cancelAnimationFrame(timeout);
+      timeout = null;
+      document.body.classList.add("exploring");
+    } else {
+      if (timeout) return;
+      startAnimation();
+      document.body.classList.remove("exploring");
+    }
+  };
+
+  window.addEventListener("scroll", onScroll);
+  onScroll();
+
+  var detailModal = aside.querySelector(".plane-detail");
+
+  aside.addEventListener("click", function(e) {
+    var image = closest(e.target, "svg[id]:not(.close-up)");
+    if (!image) return;
     var id = image.getAttribute("id").replace(stripView, "");
     var data = planeDetail[id];
-    console.log(id, data);
-  })
+    if (!data) {
+      console.log("No plane data for ", id);
+      return;
+    }
+    detailModal.innerHTML = planeHTML(data);
+    detailModal.classList.add("ready");
+    var reflow = detailModal.offsetWidth;
+    detailModal.classList.add("show");
+    var sideView = views[id].side.cloneNode(true);
+    sideView.classList.remove("side");
+    sideView.style[transform] = null;
+    sideView.classList.add("landing", "close-up");
+    detailModal.querySelector(".landing-pad").appendChild(sideView);
+    reflow = detailModal.offsetWidth;
+    sideView.classList.remove("landing");
+    sideView.onload = e => console.log(e);
+  });
+
+  detailModal.addEventListener("click", function(e) {
+    if (e.target.classList.contains("close") || e.target == detailModal) {
+      detailModal.classList.remove("ready", "show");
+    }
+  });
 
 });
